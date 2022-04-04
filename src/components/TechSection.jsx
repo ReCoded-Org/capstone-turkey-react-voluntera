456add4bd1842ce039bf518f87e619bb71734c1e
iremumut <irem2000.nur@gmail.com>
2022-04-05 02:35:49 +0300

    Add: Technologies section

---
+++ src/components/TechSection.jsx
@@ -0,0 +1,35 @@
+import TechCard from './TechCard';
+
+function TechSection() {
+  const techInfo = [
+    {
+      name: 'React Js',
+      photo:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png',
+    },
+    {
+      name: 'Tailwind CSS',
+      photo: 'https://miro.medium.com/max/712/1*Uu5RnBRw-At-D5Ko0fMf4w.png',
+    },
+    {
+      name: 'Javascript',
+      photo:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png',
+    },
+  ];
+
+  return (
+    <div className="text-center p-4 pt-12">
+      <h2 className="text-4xl text-[#026FC2] py-6">Technologies We Used</h2>
+      <div className="flex flex-row justify-around flex-wrap">
+        {techInfo.map((tech) => {
+          return (
+            <TechCard key={tech.name} name={tech.name} photo={tech.photo} />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TechSection;
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,21 +1,27 @@
 import './AboutPage.css';
+import TechSection from '../../components/TechSection';
 
 function AboutPage() {
   return (
-    <div className="flex flex-col justify-around items-center about-hero bg-cover p-4 pb-44">
-      <h1 className="text-6xl about-header">Voluntera</h1>
-      <p className="w-2/5 about-description">
-        Valuntera is a place where volunteers can find their desired
-        organization to volunteer as well as companies and organizations can
-        promote their projects and events which need volunteers.{' '}
-      </p>
-      <button
-        type="button"
-        className="bg-[#573C9B] p-3 rounded px-5 text-white get-started-button"
-      >
-        Get Started
-      </button>
-    </div>
+    <>
+      <div className="flex flex-col justify-around items-center about-hero bg-cover p-4 pb-44">
+        <h1 className="text-6xl text-[#4192d1] font-[400] leading-[63px] text-center tracking-[-1px]">
+          Voluntera
+        </h1>
+        <p className="md:w-2/5 text-center py-2 text-[#21567e] font-[400] text-2xl  tracking-[-0.5px] leading-[26px] ">
+          Valuntera is a place where volunteers can find their desired
+          organization to volunteer as well as companies and organizations can
+          promote their projects and events which need volunteers.{' '}
+        </p>
+        <button
+          type="button"
+          className="bg-[#573C9B] p-3 rounded px-5 text-white get-started-button"
+        >
+          Get Started
+        </button>
+      </div>
+      <TechSection />
+    </>
   );
 }
 
