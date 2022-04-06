a7ebc1973f61648c0e3066bd97e4abdcf730a224
ZaminMirzad <mohzamin89@gmail.com>
2022-04-06 23:36:23 +0300

    routes edited with /

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-export const HOME_ROUTE = '/';
-export const CONTACT_ROUTE = 'contact';
-export const ABOUT_ROUTE = 'about';
-export const LOGIN_ROUTE = 'login';
-export const SIGN_UP_ROUTE = 'signup';
-export const PROJECT_ROUTE = 'project';
+export const HOME_ROUTE = '/home';
+export const CONTACT_ROUTE = '/contact';
+export const ABOUT_ROUTE = '/about';
+export const LOGIN_ROUTE = '/login';
+export const SIGN_UP_ROUTE = '/signup';
+export const PROJECT_ROUTE = '/project';
 export const NOT_FOUND_ROUTE = '*';
