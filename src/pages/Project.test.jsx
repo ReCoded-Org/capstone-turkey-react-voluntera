1b8d7d603ec2989ba6931e3a467de60948d8a51d
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-04-09 19:20:11 +0300

    Chore: fix file structure

--- src/pages/Project.test.jsx
+++
@@ -1,8 +0,0 @@
-import renderer from 'react-test-renderer';
-
-import Project from './Project';
-
-test('render the project page', () => {
-  const tree = renderer.create(<Project />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
