5aeea9bd1323a70781643b08fe11e3b0c1aee324
Mohammad Zamin Mirzad <91832381+ZaminMirzad@users.noreply.github.com>
2022-04-09 18:34:29 +0300

    1 add site navbar (#9)
    * navbar added, routings applied
    
    * minor changes for responsiveness
    
    * npm install done for CI
    
    * routes, images, utils.js  local and package import separations applied as suggested
    
    * routes, images, utils.js  local and package import separations applied as suggested
    
    * routes, images, utils.js  local and package import separations and renaming applied as suggested
    
    * routes edited with /
    
    * Chore: order files
    
    * CI: fix default export error
    
    * Fix: folder name
    
    * CI: fix prettier issue
    
    * CD: fix deployment issue
    
    * Fix: import error
    
    Co-authored-by: Muhanned Noman <Muhanned.Noman@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,33 @@
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import SignUp from './pages/Signup';
+import {
+  ABOUT_ROUTE,
+  CONTACT_ROUTE,
+  HOME_ROUTE,
+  LOGIN_ROUTE,
+  NOT_FOUND_ROUTE,
+} from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path={HOME_ROUTE} element={<Home />} />
+          <Route path={ABOUT_ROUTE} element={<About />} />
+          <Route path={CONTACT_ROUTE} element={<Contact />} />
+          <Route path={LOGIN_ROUTE} element={<Login />} />
+          <Route path={NOT_FOUND_ROUTE} element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
