4bb583d1ded6a7ff6eb61e06a168e2e845aec659
ZaminMirzad <mohzamin89@gmail.com>
2022-04-09 18:24:47 +0300

    routes fixed and test render applied

---
+++ src/components/Navbar.test.jsx
@@ -0,0 +1,15 @@
+import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+
+import Navbar from './Navbar';
+
+test('render the Navbar', () => {
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
