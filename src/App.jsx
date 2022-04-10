d4f81f8b6539e9a8a0136ee791d6fd19e71a6a37
gizem <gizemc.deniz@gmail.com>
2022-04-10 18:47:42 +0300

    app js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Singin from './components/Singin';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <Singin/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
