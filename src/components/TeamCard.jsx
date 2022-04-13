51315ef08ddf503f38c4af9eac7a3c3621be4f50
iremumut <82174470+iremumut@users.noreply.github.com>
2022-04-13 18:09:44 +0300

    3 add about page (#14)
    * Add: about page hero section
    
    * Add: Technologies section
    
    * Added: team section
    
    * Add: Newsletter component
    
    * Add: email validation on newsletter
    
    * Chore: fix file structure
    
    * Merge: dev into 3-about-page
    
    Co-authored-by: Muhanned Noman <Muhanned.Noman@gmail.com>

---
+++ src/components/TeamCard.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+
+function TeamCard({ name, github, linkedin }) {
+  return (
+    <div className="basis-1/3  p-4 text-center flex flex-col align-center items-center">
+      <p className="text-lg">{name}</p>
+      <div className="flex flex-row">
+        <a href={github} rel="noopener noreferrer" className="p-2">
+          <BsGithub size={30} />
+        </a>
+        <a href={linkedin} rel="noopener noreferrer" className="p-2">
+          <BsLinkedin size={30} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default TeamCard;
+
+TeamCard.propTypes = {
+  name: PropTypes.string,
+  github: PropTypes.string,
+  linkedin: PropTypes.string,
+};
+
+TeamCard.defaultProps = {
+  name: 'Team Member',
+  github: 'github link',
+  linkedin: 'linkedin link',
+};
