c7370d6b2287701f2ba12e2d46305cc7aadc92d1
Batuhan Avcı <aavcib@adient.com>
2022-04-13 09:32:47 +0300

    Removed bootstrap and fixed Prettier issues

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
     </div>
   );
 }
--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-
-import "./ProjectCard.css";
+import './ProjectCard.css';
+import PropTypes from 'prop-types';
 
 function ProjectCard({ projectName, field, deadline }) {
   return (
     <div className="flex items-center justify-center card flex-col">
       <div id="CardDiv2" className="text-center max-w-xs w-1/2">
-        <div className="h-64 shadow-sm bg-slate-300"></div>
-        <h2 className="text-lg text-left text-blue-600 pt-5 text-center "> {projectName} </h2>
+        <div className="h-64 shadow-sm bg-slate-300" />
+        <h2 className="text-lg text-left text-blue-600 pt-5 text-center ">
+          {' '}
+          {projectName}{' '}
+        </h2>
         <div id="FieldAndDeadlineDivs">
-          <div className={`text-sm ${deadline != null ? "text-left" : "text-center" } text-gray-400 pl-5 pb-5`}>
+          <div
+            className={`text-sm ${
+              deadline != null ? 'text-left' : 'text-center'
+            } text-gray-400 pl-5 pb-5`}
+          >
             {field}
           </div>
-          <div id="DeadlineDiv" className="text-sm text-left text-gray-400 pl-5 pb-5">
+          <div
+            id="DeadlineDiv"
+            className="text-sm text-left text-gray-400 pl-5 pb-5"
+          >
             {deadline}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ProjectCard;
+
+ProjectCard.propTypes = {
+  projectName: PropTypes.string,
+  field: PropTypes.string,
+  deadline: PropTypes.string,
+};
+
+ProjectCard.defaultProps = {
+  projectName: 'Name',
+  field: 'Field',
+  deadline: 'Deadline',
+};
