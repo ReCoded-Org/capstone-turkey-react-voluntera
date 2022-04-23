9e616f1fd67cee45f18c608a89d31e54bc1c5926
Zamin Mirzad <mohzamin89@gmail.com>
2022-04-23 14:26:42 +0300

    added projectsSlice, fetched data to it and display on homepage click on apply navigates to details of that project

--- src/components/ProjectContent.jsx
+++ src/components/ProjectContent.jsx
@@ -1,34 +1,30 @@
-function ProjectContent({ project }) {
-  return project.map((p) => {
-    return (
-      <div
-        key={p.id}
-        className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-8  p-3"
-      >
-        <div className="flex flex-col p-3 ">
-          <img
-            src={`${p.image}`}
-            alt="project"
-            className="sm:h-48 sm:w-auto md:w-auto lg:w-80  rounded"
-          />
-          <h1 className="text-center sm:text-center md:text-left lg:text-left text-2xl text-blue-500 py-3 font-normal">
-            {p.name}
+import PropTypes from 'prop-types';
+
+function ProjectContent({ title, type, address, date, pic, skills, isOpen }) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8  p-3">
+      <div className="flex flex-col p-3">
+        <img
+          src={pic}
+          alt="project"
+          className="sm:h-48 sm:w-auto md:w-auto lg:w-full  rounded border-b-2"
+        />
+        <div className="flex flex-col">
+          <h1 className="text-center sm:text-center md:text-left lg:text-left text-2xl text-blue-600 py-3 font-normal">
+            Name: {title}
           </h1>
-          <span className="text-sm text-center sm:text-center md:text-left lg:text-left">
-            {p.date}
+          <span className="text-blue-600 text-center sm:text-center md:text-left lg:text-left">
+            Deadline: {date.slice(0, 10)}
           </span>
-          <span className="text-center sm:text-center md:text-left lg:text-left pb-4">
-            {p.time}
+          <span className="text-slate-700 text-center sm:text-center md:text-left lg:text-left pb-4">
+            Type: {type}
           </span>
-          <div className="flex flex-col items-center lg:flex md:flex">
-            {p.description}
+          <span className=" flex gap-2 text-center sm:text-center md:text-left lg:text-left text-lg text-slate-700">
+            <span className="text-blue-600">Status:</span>
+            {isOpen ? 'Open' : 'Closed'}
+          </span>
+          <div className="flex flex-col items-center">
             <div className="flex w-60 justify-between pt-8">
-              <div className="flex flex-col">
-                <span className="flex flex-col text-2xl text-blue-700 ">
-                  {p.applicants}{' '}
-                  <span className="text-xs text-gray-400 ">Applicants</span>
-                </span>
-              </div>
               <button
                 type="button"
                 className="w-28 h-10 border bg-purple-500 px-8 rounded-full flex items-center justify-center text-white text-sm font-bold hover:text-purple-500 hover:bg-purple-300 hover:border hover:border-purple-500"
@@ -38,19 +34,38 @@ function ProjectContent({ project }) {
             </div>
           </div>
         </div>
-        <div className="md:flex md:flex-col md:justify-center p-3 md:pt-28 ">
-          <p>
-            <span className="text-blue-600 font-bold">Skills: </span>
-            {p.skills}
-          </p>
-          <p>
-            <span className="text-blue-600 font-bold">Address: </span>
-            {p.address}
-          </p>
-        </div>
       </div>
-    );
-  });
+      <div className="md:flex md:flex-col md:justify-center p-3 md:pt-28 lg:col-span-2">
+        <p className="text-slate-800">
+          <span className="text-blue-600 font-bold">Skills: </span>
+          {skills.join(' + ').toUpperCase()}
+        </p>
+        <p>
+          <span className="text-blue-600 font-bold">Address: </span>
+          {address}
+        </p>
+      </div>
+    </div>
+  );
 }
 
+ProjectContent.propTypes = {
+  title: PropTypes.string,
+  date: PropTypes.string,
+  type: PropTypes.string,
+  address: PropTypes.string,
+  pic: PropTypes.string,
+  skills: PropTypes.instanceOf(Array),
+  isOpen: PropTypes.bool,
+};
+
+ProjectContent.defaultProps = {
+  title: '',
+  date: null,
+  type: '',
+  address: '',
+  pic: 'https://retaintechnologies.com/wp-content/uploads/2020/04/Project-Management-Mantenimiento-1.jpg',
+  skills: [],
+  isOpen: null,
+};
 export default ProjectContent;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,50 +1,44 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
-import '../../components/ProjectCard/ProjectCard.css';
+import { getProjects } from '../../redux/slices/projectsSlice';
 
 export default function Home() {
-  const exampleData = [
-    {
-      projectName: 'Project Name 1',
-      field: 'Field 1',
-      deadline: 'Deadline 12/05/2022',
-    },
-    {
-      projectName: 'Project Name 2',
-      field: 'Field 2',
-      deadline: 'Deadline 13/05/2022',
-    },
-    {
-      projectName: 'Project Name 3',
-      field: 'Field 3',
-    },
-    {
-      projectName: 'Project Name 4',
-      field: 'Field 4',
-      deadline: 'Deadline 14/05/2022',
-    },
-  ];
+  const projects = useSelector((state) => state.projects.list);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProjects());
+  }, [dispatch]);
+
   return (
-    <div>
-      <div className="AvailableProjectsH1">
-        <h1>Available Projects</h1>
-      </div>
-      <div className="FilterButtons">
-        <button type="button">All Projects</button>
-        <button type="button">Recent Projects</button>
+    <div className="py-10 px-16">
+      <h1 className="text-3xl text-blue-500 px-16">Available Projects</h1>
+
+      <div className="px-16 flex gap-3 text-blue-500 mt-4 font-bold">
+        <button
+          type="button"
+          className="bg-blue-700 text-white px-3 py-2 rounded-md font-bold"
+        >
+          All Projects
+        </button>
+        <button type="button" onClick={() => dispatch(getProjects())}>
+          Recent Projects
+        </button>
         <button type="button">Social Events</button>
         <button type="button">some filter</button>
         <button type="button">some filter</button>
       </div>
-      <div
-        id="ProjectSection"
-        className="flex flex-row   justify-center flex-wrap"
-      >
-        {exampleData.map((data) => (
+      <div className="flex gap-5 justify-center flex-wrap my-5 ">
+        {projects?.map((data) => (
           <ProjectCard
-            key={data.projectName}
-            projectName={data.projectName}
-            field={data.field}
-            deadline={data.deadline}
+            /* eslint no-underscore-dangle: 0 */
+            key={data._id}
+            id={data._id}
+            projectName={data.title}
+            field={data.type}
+            deadline={data.date.slice(0, 7)}
           />
         ))}
       </div>
