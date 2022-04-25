6413d2ac7b463470e23151eeb617e14603b43419
Zamin Mirzad <mohzamin89@gmail.com>
2022-04-25 23:12:04 +0300

    minor edits on signup inputs

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -12,7 +12,7 @@ export default function InputField({
     <div>
       <label
         htmlFor={id}
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+        className="block mb-2 text-sm font-medium text-gray-900 "
       >
         {label}
       </label>
@@ -24,7 +24,7 @@ export default function InputField({
           type={type}
           id={id}
           name={id}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5    "
           placeholder={placeholder}
           onChange={(e) => handleChange(e)}
         />
