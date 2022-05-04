72d06bb8701317144a037c6c06c7ec4687a418d8
Zamin Mirzad <mohzamin89@gmail.com>
2022-05-04 16:00:38 +0300

    some code restructure and fixing

--- src/pages/User/User.test.jsx
+++ src/pages/User/User.test.jsx
@@ -1,8 +1,16 @@
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 
+import store from '../../redux/store/store';
 import User from './User';
 
 test('render the Userprofile page', () => {
-  const tree = renderer.create(<User />).toJSON();
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <User />
+      </Provider>,
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
